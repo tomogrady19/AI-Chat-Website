31b44e08dd68f08524c1ca9d3fe9de61f2edf631
Thomas O'Grady <tomogrady19@gmail.com>
2025-12-17 13:20:22 +0000

    Spotify OAuth added (redirects you to login etc). Took longer than it looks :/

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+console.log("SERVER STARTED", Date.now());
+
 import OpenAI from "openai";
 import express from "express";
 import "dotenv/config";
 import rateLimit from "express-rate-limit";
+import session from "express-session";
+import crypto from "crypto";
 
 const rateLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
@@ -15,6 +19,7 @@ const client = new OpenAI({
 });
 
 const app = express();
+app.use(session({secret: "dev-secret-change-later", resave: false, saveUninitialized: false, cookie: {sameSite: "lax"}}));
 app.use(express.json()); // allows server to understand json files
 app.use(express.static("public")); // serves your frontend files in the "public" folder
 
@@ -44,8 +49,52 @@ app.post("/api/ask", rateLimiter, async (req, res) => {
     }
 });
 
+app.get("/auth/spotify/login", (req, res) => {
+    const state = crypto.randomBytes(16).toString("hex");
+    req.session.spotifyState = state;
+
+    const scope = [
+        "user-top-read",
+        "playlist-read-private",
+        "playlist-read-collaborative"
+        ].join(" ");
+
+    const params = new URLSearchParams({
+        response_type: "code",
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope,
+        redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+        state
+    });
+
+    res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
+
+    //debugging
+    console.log("state:", state);
+    console.log("ID:", req.sessionID);
+
+});
+
+app.get("/auth/spotify/callback", (req, res) => {
+    const { code, state } = req.query;
+
+    //debugging
+    console.log("state:", state);
+    console.log("ID:", req.sessionID);
+    console.log("spotify state:", req.session.spotifyState);
+
+    if (!state || state !== req.session.spotifyState) {
+        return res.status(400).send("State mismatch");
+    }
+
+    // temporary message
+    res.send("Spotify auth successful");
+});
+
+
 app.listen(3000, () =>
-    console.log("Running at http://localhost:3000")
+    console.log("Running at http://127.0.0.1:3000")
 );
 
+console.log("Test Spotify OAuth at http://127.0.0.1:3000/auth/spotify/login");
 // node index.js
