0a050a949dc753414d5d97ae08c3a2a9b7926fd6
Thomas O'Grady <tomogrady19@gmail.com>
2025-12-31 15:43:42 +0000

    minor refactor: unused functions removed #2

--- public/js/ui/clear.js
+++ public/js/ui/clear.js
@@ -11,24 +11,6 @@ function clearProfile() {
     if (profile) profile.innerHTML = "";
 }
 
-function clearNowPlaying() {
-    const nowPlaying = document.getElementById("now-playing");
-    if (nowPlaying) nowPlaying.textContent = "";
-}
-
-function clearTogglePlay() {
-    const togglePlay = document.getElementById("toggle-play");
-    if (togglePlay) togglePlay.hidden = true;
-}
-
-function clearTimeRange() {
-    const timeRangeLabel = document.querySelector('label[for="timeRange"]');
-    const timeRangeSelect = document.getElementById("timeRange");
-
-    timeRangeLabel.hidden = true;
-    timeRangeSelect.hidden = true;
-}
-
 function clearMainButtons() {
     document.getElementById("now-playing").textContent = ""; //clear current song name
     document.getElementById("mainButtons").style.display = "none"; //hide everything in mainButtons class
