b7bc629a2d7b15bd6e02165e426783e745df90e5
Thomas O'Grady <tomogrady19@gmail.com>
2026-01-14 15:45:21 +0000

    fixed bug: extra )

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`));
+  console.log(`Backend running on port ${PORT}`);
 });
 
 // npm run start
