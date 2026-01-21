3761255db6750ba0e419344bef74233892ed7fa3
Thomas O'Grady <tomogrady19@gmail.com>
2026-01-21 11:43:25 +0000

    no longer exposing JWT creation errors to client

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -2,7 +2,12 @@ import jwt from "jsonwebtoken";
 
 // issue a JWT if user logs into Spotify
 export function issueJwt({ spotifyId }) {
-    if (!spotifyId) throw new Error("Missing spotifyId for JWT");
+    if (!spotifyId) {
+        const err = new Error("Internal auth error");
+        err.status = 500;
+        err.expose = false;
+        throw err;
+    }
 
     return jwt.sign(
         { spotifyId },
